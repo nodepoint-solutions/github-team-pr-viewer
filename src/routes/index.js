6b4016538c549e507479e8ecf1daedeca23ffd3a
Alex Luckett <alexgluckett@gmail.com>
2026-03-26 11:33:10 +0000

    fix: hoist nunjucks macro imports to top level; build dropdown options from filtered PRs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,20 @@
-export default { method: 'GET', path: '/', handler: (_, h) => h.response('stub').code(200) }
+import { getPRs, isBot } from '../services/prs.js'
+import { applyFilters, applySort, buildViewContext } from './helpers.js'
+
+export default {
+  method: 'GET',
+  path: '/',
+  async handler(request, h) {
+    const { repo = '', author = '', sort = 'updated', dir = 'desc', cooldown } = request.query
+    const cooldownFlag = cooldown === '1'
+    const data = await getPRs()
+
+    const basePRs = data.prs.filter(
+      (pr) => data.teamMembers.has(pr.author) && !isBot({ type: pr.authorType, login: pr.author })
+    )
+
+    const prs = applySort(applyFilters(basePRs, { repo, author }), sort, dir)
+
+    return h.view('index', buildViewContext(data, prs, prs, { repo, author, sort, dir }, '/', 'Team PRs', 'Pull requests opened by members of the DEFRA/forms team.', cooldownFlag))
+  },
+}
